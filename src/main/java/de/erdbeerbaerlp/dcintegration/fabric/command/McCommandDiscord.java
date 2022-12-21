43a8ac89f954588e1b425c8a1723ad0f5bef73f8
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2022-12-21 19:02:36 +0100

    Update to 2.6.2

--- src/main/java/de/erdbeerbaerlp/dcintegration/fabric/command/McCommandDiscord.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/fabric/command/McCommandDiscord.java
@@ -2,18 +2,12 @@ package de.erdbeerbaerlp.dcintegration.fabric.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import de.erdbeerbaerlp.dcintegration.common.addon.AddonLoader;
+import de.erdbeerbaerlp.dcintegration.common.minecraftCommands.MCSubCommand;
+import de.erdbeerbaerlp.dcintegration.common.minecraftCommands.McCommandRegistry;
 import de.erdbeerbaerlp.dcintegration.common.storage.Configuration;
-import de.erdbeerbaerlp.dcintegration.common.storage.Localization;
-import de.erdbeerbaerlp.dcintegration.common.storage.PlayerLinkController;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.*;
-import net.minecraft.util.Formatting;
-
-import java.io.IOException;
-
-import static de.erdbeerbaerlp.dcintegration.common.util.Variables.discord_instance;
 
 public class McCommandDiscord {
     public McCommandDiscord(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -24,33 +18,9 @@ public class McCommandDiscord {
                             .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Configuration.instance().ingameCommand.inviteURL))), false);
             return 0;
         });
-        l.then(CommandManager.literal("ignore").executes((ctx) -> {
-            ctx.getSource().sendFeedback(
-                    Text.of(discord_instance.togglePlayerIgnore(ctx.getSource().getPlayer().getUuid()) ? Localization.instance().commands.commandIgnore_unignore : Localization.instance().commands.commandIgnore_ignore), true);
-            return 0;
-        })).then(CommandManager.literal("link").executes((ctx) -> {
-            if (Configuration.instance().linking.enableLinking && discord_instance.srv.isOnlineMode() && !Configuration.instance().linking.whitelistMode) {
-                if (PlayerLinkController.isPlayerLinked(ctx.getSource().getPlayer().getUuid())) {
-                    ctx.getSource().sendFeedback(Text.of(Formatting.RED + Localization.instance().linking.alreadyLinked.replace("%player%", discord_instance.getJDA().getUserById(PlayerLinkController.getDiscordFromBedrockPlayer(ctx.getSource().getPlayer().getUuid())).getAsTag())), false);
-                    return 0;
-                }
-                final int r = discord_instance.genLinkNumber(ctx.getSource().getPlayer().getUuid());
-                ctx.getSource().sendFeedback(Texts.setStyleIfAbsent(Text.literal(Localization.instance().linking.linkMsgIngame.replace("%num%", r + "").replace("%prefix%", "/")), Style.EMPTY.withFormatting(Formatting.AQUA).withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "" + r)).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal(Localization.instance().linking.hoverMsg_copyClipboard)))), false);
-            } else {
-                ctx.getSource().sendFeedback(Text.of(Formatting.RED + Localization.instance().commands.subcommandDisabled), false);
-            }
-            return 0;
-        })).then(CommandManager.literal("reload").requires((p) -> p.hasPermissionLevel(4)).executes((ctx) -> {
-            try {
-                Configuration.instance().loadConfig();
-            } catch (IOException e) {
-                ctx.getSource().sendFeedback(Texts.setStyleIfAbsent(Text.literal(e.getMessage()),Style.EMPTY.withFormatting(Formatting.RED)),true);
-                e.printStackTrace();
-            }
-            AddonLoader.reloadAll();
-            ctx.getSource().sendFeedback(Text.of(Localization.instance().commands.configReloaded), true);
-            return 0;
-        }));
+        for (MCSubCommand cmd : McCommandRegistry.getCommands()) {
+            l.then(CommandManager.literal(cmd.getName()));
+        }
         dispatcher.register(l);
     }
 }
---
+++ src/main/java/de/erdbeerbaerlp/dcintegration/fabric/mixin/CommandManagerMixin.java
@@ -0,0 +1,115 @@
+package de.erdbeerbaerlp.dcintegration.fabric.mixin;
+
+import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dcshadow.net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import de.erdbeerbaerlp.dcintegration.common.minecraftCommands.MCSubCommand;
+import de.erdbeerbaerlp.dcintegration.common.minecraftCommands.McCommandRegistry;
+import de.erdbeerbaerlp.dcintegration.common.storage.Configuration;
+import de.erdbeerbaerlp.dcintegration.common.storage.Localization;
+import de.erdbeerbaerlp.dcintegration.common.util.DiscordMessage;
+import de.erdbeerbaerlp.dcintegration.common.util.MessageUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.apache.commons.lang3.ArrayUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static de.erdbeerbaerlp.dcintegration.common.util.Variables.discord_instance;
+
+@Mixin(CommandManager.class)
+public class CommandManagerMixin {
+
+    @Inject(method = "execute", cancellable = true, at = @At("HEAD"))
+    public void execute(ParseResults<ServerCommandSource> parseResults, String command, CallbackInfoReturnable<Integer> cir){
+
+        final ServerCommandSource source = parseResults.getContext().getSource();
+        command = command.replaceFirst(Pattern.quote("/"), "");
+        if (!Configuration.instance().commandLog.channelID.equals("0")) {
+            if (!ArrayUtils.contains(Configuration.instance().commandLog.ignoredCommands, command.split(" ")[0]))
+                discord_instance.sendMessage(Configuration.instance().commandLog.message
+                        .replace("%sender%", source.getName())
+                        .replace("%cmd%", command)
+                        .replace("%cmd-no-args%", command.split(" ")[0]), discord_instance.getChannel(Configuration.instance().commandLog.channelID));
+        }
+        if (discord_instance != null) {
+            boolean raw = false;
+
+            if (((command.startsWith("say")) && Configuration.instance().messages.sendOnSayCommand) || (command.startsWith("me") && Configuration.instance().messages.sendOnMeCommand)) {
+                String msg = command.replace("say ", "");
+                if (command.startsWith("say"))
+                    msg = msg.replaceFirst("say ", "");
+                if (command.startsWith("me")) {
+                    raw = true;
+                    msg = "*" + MessageUtils.escapeMarkdown(msg.replaceFirst("me ", "").trim()) + "*";
+                }
+                final Entity sourceEntity = source.getEntity();
+                discord_instance.sendMessage(source.getName(), sourceEntity != null ? sourceEntity.getUuid().toString() : "0000000", new DiscordMessage(null, msg, !raw), discord_instance.getChannel(Configuration.instance().advanced.chatOutputChannelID));
+            }
+
+            if (command.startsWith("discord ") || command.startsWith("dc ")) {
+                final String[] args = command.replace("discord ", "").replace("dc ", "").split(" ");
+                for (MCSubCommand mcSubCommand : McCommandRegistry.getCommands()) {
+                    if (args[0].equals(mcSubCommand.getName())) {
+                        final String[] cmdArgs = args.length > 1 ? Arrays.copyOfRange(args, 1, args.length) : new String[0];
+                        switch (mcSubCommand.getType()) {
+                            case CONSOLE_ONLY:
+                                try {
+                                    source.getPlayerOrThrow();
+                                    source.sendError(Text.literal(Localization.instance().commands.consoleOnly));
+                                } catch (CommandSyntaxException e) {
+                                    final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, null));
+                                    source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                }
+                                break;
+                            case PLAYER_ONLY:
+                                try {
+                                    final ServerPlayerEntity player = source.getPlayerOrThrow();
+                                    if (!mcSubCommand.needsOP()) {
+                                        final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, player.getUuid()));
+                                        source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                    } else if (source.hasPermissionLevel(4)) {
+                                        final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, player.getUuid()));
+                                        source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                    } else {
+                                        source.sendError(Text.literal(Localization.instance().commands.noPermission));
+                                    }
+                                } catch (CommandSyntaxException e) {
+                                    source.sendError(Text.literal(Localization.instance().commands.ingameOnly));
+
+                                }
+                                break;
+                            case BOTH:
+
+                                try {
+                                    final ServerPlayerEntity player = source.getPlayerOrThrow();
+                                    if (!mcSubCommand.needsOP()) {
+                                        final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, player.getUuid()));
+                                        source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                    } else if (source.hasPermissionLevel(4)) {
+                                        final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, player.getUuid()));
+                                        source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                    } else {
+                                        source.sendError(Text.literal(Localization.instance().commands.noPermission));
+                                    }
+                                } catch (CommandSyntaxException e) {
+                                    final String txt = GsonComponentSerializer.gson().serialize(mcSubCommand.execute(cmdArgs, null));
+                                    source.sendFeedback(Text.Serializer.fromJson(txt), false);
+                                }
+                                break;
+                        }
+                    }
+                    cir.setReturnValue(1);
+                }
+            }
+        }
+    }
+}
